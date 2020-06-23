f07016bd86b2239c848ccb7602255651e2d64b13
cxt <hb_cxtl@163.com>
2020-06-23 10:01:00 +0800

    新增 store js 代码

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ import '@/icons' // icon
 Vue.use(ElementUI)
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
